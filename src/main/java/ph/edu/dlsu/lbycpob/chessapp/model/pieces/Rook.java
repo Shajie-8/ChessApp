f539d287ee336aa4fed9ace6f57e58b89543b9a4
Shajie-8 <shajie.macabalang@gmail.com>
2026-06-22 21:51:40 +0800

    Added Check destination for rook

--- src/main/java/ph/edu/dlsu/lbycpob/chessapp/model/pieces/Rook.java
+++ src/main/java/ph/edu/dlsu/lbycpob/chessapp/model/pieces/Rook.java
@@ -67,6 +67,9 @@ public class Rook extends ChessPiece {
             currCol += colStep;
         }
 
+        ChessPiece target = board.pieceAt(row, col);
+        if (target != null && target.getColor() == this.color) return false;
+
         return !moveWouldCauseCheck(row, col, board);
     }
 
